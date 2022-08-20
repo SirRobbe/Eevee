ccb795f82feda9e04d1901848dfd94ac4b26e3de
SirRobbe <sirrobbe@protonmail.com>
2022-08-20 10:30:15 +0200

    Fixed bug with unneccessary whitespace before return type of function

--- eevee/C++/Function.cs
+++ eevee/C++/Function.cs
@@ -1,32 +1,34 @@
+using System.Text;
+
 namespace Eevee;
 
 public partial class CppWriter
 {
-    public class Function : IDisposable
+    public sealed class Function : IDisposable
     {
         public Function(CodeWriter writer, string name, string returnType = "void",
                         string prefix = "",
                         string[] arguments = null)
         {
             string args = arguments == null ? "" : string.Join(", ", arguments);
+
+            var builder = new StringBuilder(64);
+
+            if(!string.IsNullOrEmpty(prefix))
+            {
+                builder.Append($"{prefix} ");
+            }
+
+            builder.Append($"{returnType} {name}({args})");
             
             m_Writer = writer;
-            m_Writer.WriteLine($"{prefix} {returnType} {name}({args})");
+            m_Writer.WriteLine(builder.ToString());
             m_Writer.BeginBlock();
-        }
-        
-        protected virtual void Dispose(bool disposing)
-        {
-            if(disposing)
-            {
-                m_Writer.EndBlock();
-            }
         }
 
         public void Dispose()
         {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            m_Writer.EndBlock();
         }
 
         private readonly CodeWriter m_Writer;
